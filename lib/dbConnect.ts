c2604fbd2dbd8dd0c0346e7d4cae77d83ea0e26c
coenhewes <coenhewes@proton.me>
2024-09-01 22:27:51 +1000

    Declared type for mongoose

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -1,6 +1,14 @@
 // lib/dbConnect.ts
 import mongoose from 'mongoose';
 
+
+declare global {
+  var mongoose: {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+  } | undefined;
+}
+
 const MONGODB_URI = process.env.MONGODB_URI!;
 
 if (!MONGODB_URI) {
