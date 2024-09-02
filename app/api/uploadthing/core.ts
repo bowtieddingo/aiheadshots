1f99a6c6a4abcf9311f9f78e2f14866949c93d43
coenhewes <coenhewes@proton.me>
2024-09-02 17:59:02 +1000

    Auth.ts in core.ts for uploadthing

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,7 +1,7 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "../auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
 
 const f = createUploadthing();
 
